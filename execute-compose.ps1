80143f4afe6c0de67ac09180e65092f9144ec9d5
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-07 16:39:44 -0200

    update readme

--- execute-compose.ps1
+++ execute-compose.ps1
@@ -1,1 +1,3 @@
+Invoke-WebRequest https://dl.bintray.com/docker-compose/master/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile $env:ProgramFiles\docker\docker-compose.exe 
+
 docker-compose up
