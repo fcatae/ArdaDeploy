b5fc5aa60c3f0de2bcf1f0ff0134e090b4ccbe5c
Fabricio Catae <fabricio.catae@microsoft.com>
2017-02-07 18:48:15 -0200

    disable antivirus to see if it deploys faster

--- execute-compose.ps1
+++ execute-compose.ps1
@@ -1,3 +1,8 @@
 Invoke-WebRequest https://dl.bintray.com/docker-compose/master/docker-compose-Windows-x86_64.exe -UseBasicParsing -OutFile $env:ProgramFiles\docker\docker-compose.exe 
 
+Set-MpPreference -DisableRealtimeMonitoring $true 
+
+docker-compose pull
 docker-compose up -d
+
+Set-MpPreference -DisableRealtimeMonitoring $false 
